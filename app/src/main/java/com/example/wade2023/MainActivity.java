3a3f19db728ace5df6ca6e62224de5bf711b09eb
Admin <ibraheemafifi02@gmail.com>
2023-09-14 15:00:09 +0300

    mn

--- app/src/main/java/com/example/wade2023/MainActivity.java
+++ app/src/main/java/com/example/wade2023/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.wade2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("WH","onCreate");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("WH","onRestart");
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("WH","onResume");
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
     }
 }
