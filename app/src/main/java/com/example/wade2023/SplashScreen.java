6070a7292511bbd5e3c0f3c42209f1d7990b857a
Admin <ibraheemafifi02@gmail.com>
2023-11-05 10:02:03 +0200

    mn

---
+++ app/src/main/java/com/example/wade2023/SplashScreen.java
@@ -0,0 +1,68 @@
+package com.example.wade2023;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Log.d(" ", "OnCreate:");
+        Toast.makeText(this, "OnCreate:", Toast.LENGTH_SHORT).show();
+        //بناء قاعدة بيانات وارجاع مؤشر عليها 1
+
+        Handler h=new Handler();
+        Runnable r=new Runnable() {
+
+            public void run()
+            {
+                //to open new activity from current to next
+                Intent i= new Intent(SplashScreen.this, SignInActivity.class);
+                startActivity(i);
+                //to close current activity
+                finish();
+            }
+        };
+        h.postDelayed(r,3000);
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("yassen", "onRestart: ");
+        Toast.makeText(this, "onRestart:",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("yassen", "onResume: ");
+        Toast.makeText(this, "onResume:",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("yassen", "onPause: ");
+        Toast.makeText(this, "onPause:",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("yassen", "onStop: ");
+        Toast.makeText(this, "onStop:",Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("yassen", "onDestroy: ");
+        Toast.makeText(this, "onDestroy:",Toast.LENGTH_SHORT).show();
+    }
+}
