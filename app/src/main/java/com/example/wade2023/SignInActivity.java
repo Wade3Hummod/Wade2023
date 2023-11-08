4f146a10534a3d9b0ab6010645940de9b5e49cba
Admin <ibraheemafifi02@gmail.com>
2023-11-08 14:12:15 +0200

    to do

--- app/src/main/java/com/example/wade2023/SignInActivity.java
+++ app/src/main/java/com/example/wade2023/SignInActivity.java
@@ -2,7 +2,9 @@ package com.example.wade2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -15,5 +17,19 @@ public class SignInActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+    }
+    public void onclickGo1(View v)
+    {
+
+        Intent i= new Intent(SignInActivity.this, SignUpActivity.class);
+        startActivity(i);
+        finish();
+    }
+    public void onclickGo2(View v)
+    {
+
+        Intent i= new Intent(SignInActivity.this, SignInActivity.class);
+        startActivity(i);
+        finish();
     }
 }
