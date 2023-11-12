287112b6e1eeba2f26b55e522d181dd623b345e1
Admin <ibraheemafifi02@gmail.com>
2023-11-12 09:40:30 +0200

    to do

--- app/src/main/java/com/example/wade2023/SignInActivity.java
+++ app/src/main/java/com/example/wade2023/SignInActivity.java
@@ -5,17 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 public class SignInActivity extends AppCompatActivity
 {
 
+
     private TextInputEditText etEmail;
+    private TextInputEditText etPassword;
+    private Button btnSignIn2;
+    private Button btnSingUp2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        etPassword=findViewById(R.id.etPassword);
+        etEmail=findViewById(R.id.etEmail);
+        btnSingUp2=findViewById(R.id.btnSingUp2);
+        btnSignIn2=findViewById(R.id.btnSignIn2);
+
+
+
+
 
     }
     public void onclickGo1(View v)
@@ -28,8 +42,7 @@ public class SignInActivity extends AppCompatActivity
     public void onclickGo2(View v)
     {
 
-        Intent i= new Intent(SignInActivity.this, SignInActivity.class);
-        startActivity(i);
-        finish();
+
     }
+
 }
--- app/src/main/java/com/example/wade2023/SignUpActivity.java
+++ app/src/main/java/com/example/wade2023/SignUpActivity.java
@@ -3,12 +3,36 @@ package com.example.wade2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+
+public class SignUpActivity extends AppCompatActivity
+{
+    private Button btnSave;
+    private Button btnCancel;
+    private TextInputEditText etName;
+    private TextInputEditText etPhone;
+    private TextInputEditText etEmail;
+    private TextInputEditText etText;
+    private TextInputEditText etRe_password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        btnSave=findViewById(R.id.btnSave);
+        btnCancel=findViewById(R.id.btnCancel);
+        etName=findViewById(R.id.etName);
+        etPhone=findViewById(R.id.etPhone);
+        etEmail=findViewById(R.id.etEmail);
+        etText=findViewById(R.id.etText);
+        etRe_password=findViewById(R.id.etRe_password);
+
+
+
+
+
     }
 }
