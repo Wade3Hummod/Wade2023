ecfb167c8495bb5cedbbec76db3b717e01273939
Admin <ibraheemafifi02@gmail.com>
2023-11-14 12:22:46 +0200

    to do

---
+++ app/src/main/java/com/example/wade2023/EditTaskActivity.java
@@ -0,0 +1,33 @@
+package com.example.wade2023;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class EditTaskActivity extends AppCompatActivity
+{
+    private Button btnUpdate;
+    private Button btnCancel;
+    private TextView tvImportance;
+    private SeekBar skbrImportance;
+    private TextInputEditText etShortTitle;
+    private TextInputEditText etText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_task);
+        btnUpdate=findViewById(R.id.btnUpdate);
+        btnCancel=findViewById(R.id.btnCancel);
+        tvImportance=findViewById(R.id.tvImportance);
+        skbrImportance=findViewById(R.id.skbrImportance);
+        etShortTitle=findViewById(R.id.etShortTitle);
+        etText=findViewById(R.id.etText);
+    }
+
+}
--- app/src/main/java/com/example/wade2023/SplashScreen.java
+++ app/src/main/java/com/example/wade2023/SplashScreen.java
@@ -1,5 +1,4 @@
 package com.example.wade2023;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
