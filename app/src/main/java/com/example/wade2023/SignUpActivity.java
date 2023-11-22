ef9d9426940824b015248e9e6865207c390cd520
Admin <ibraheemafifi02@gmail.com>
2023-11-22 11:36:52 +0200

    to do

--- app/src/main/java/com/example/wade2023/SignUpActivity.java
+++ app/src/main/java/com/example/wade2023/SignUpActivity.java
@@ -10,11 +10,9 @@ import android.widget.Toast;
 import com.example.wade2023.data.AppDatabase;
 import com.example.wade2023.data.usersTable.MyUser;
 import com.example.wade2023.data.usersTable.MyUserQuery;
-import com.example.wade2023.data.usersTable.MyUserQuery_Impl;
 import com.google.android.material.textfield.TextInputEditText;
 
-public class SignUpActivity extends AppCompatActivity
-{
+public class SignUpActivity extends AppCompatActivity {
     private Button btnSave;
     private Button btnCancel;
     private TextInputEditText etName;
@@ -28,31 +26,27 @@ public class SignUpActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        btnSave=findViewById(R.id.btnSave);
-        btnCancel=findViewById(R.id.btnCancel);
-        etName=findViewById(R.id.etName);
-        etPhone=findViewById(R.id.etPhone);
-        etEmail=findViewById(R.id.etEmail);
-        etPassword=findViewById(R.id.etPassword);
-        etRe_password=findViewById(R.id.etRe_password);
-
-
-
+        btnSave = findViewById(R.id.btnSave);
+        btnCancel = findViewById(R.id.btnCancel);
+        etName = findViewById(R.id.etName);
+        etPhone = findViewById(R.id.etPhone);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        etRe_password = findViewById(R.id.etRe_password);
 
 
     }
-    public void onclickCancel(View v)
-    {
+
+    public void onclickCancel(View v) {
         finish();
 
     }
-    public void onclickSave(View v)
-    {
+
+    public void onclickSave(View v) {
         checkSignUpSave();
     }
 
-    private void checkSignUpSave()
-    {
+    private void checkSignUpSave() {
         boolean isAllok = true; //يفحص الحقول ان كانت سليمة
         //استخراج النص من حقل الايميل
         String email = etEmail.getText().toString();
@@ -73,19 +67,22 @@ public class SignUpActivity extends AppCompatActivity
             etEmail.setError("Wrong Email");
 
         }
-        if (password.length() < 8 && password.length() > 20 || password.contains("") == true) {
+        if (password.length() > 8 && password.length() < 20 || password.contains("") == true)
+        {
             isAllok = false;
             etPassword.setError("Password between 8 - 20 letters");
         }
-        if (!repassword.equals(password)) {
+        if (!repassword.equals(password))
+        {
             isAllok = false;
             etRe_password.setError("should be the same password");
         }
 
 
-        if (phone.length() > 10 || phone.contains("") == true) {
+        if (phone.length() <10 || phone.contains("") == true)
+        {
             isAllok = false;
-            etPhone.setError("phone number is 10 numbers");
+            etPhone.setError("phone number is not 10  numbers");
 
         }
 
@@ -93,25 +90,26 @@ public class SignUpActivity extends AppCompatActivity
         if (isAllok) {
             Toast.makeText(this, "All Ok", Toast.LENGTH_SHORT).show();
             AppDatabase db = AppDatabase.getDB(getApplicationContext());
-            MyUserQuery usersQuery=db.getMyUserQuery();
+            MyUserQuery usersQuery = db.getMyUserQuery();
             //فحص هل البريد الالكتروني موجود من قبل اي تم تسجيل من قبل
-            if(usersQuery.checkEmail(email)!=null){
+            if (usersQuery.checkEmail(email) != null) {
                 etEmail.setError("found Email");
-            }
-            else// ان لم يكن موجودا نقوم ببناء كاءن للمستعمل وادخاله في الجدول Myuser المستعملين
+            } else// ان لم يكن موجودا نقوم ببناء كاءن للمستعمل وادخاله في الجدول Myuser المستعملين
             {
                 // بناء الكائن
-                MyUserQuery myuser= new myuser();
+                MyUser myUser = new MyUser();
                 //تحديد قيم الصفات بالقيم التي استخرجناها
-                myuser.email=email;
-                myuser.fullName=name;
-                myuser.phone=phone;
-                myuser.passw=password;
+                myUser.email = email;
+                myUser.fullName = name;
+                myUser.phone = phone;
+                myUser.passw = password;
                 //اضافه الكائن الجديد للجدول
-                usersQuery.insert(myuser);
+                usersQuery.insert(myUser);
                 //اغلاق الشاشه الحالية
                 finish();
 
 
             }
+        }
+    }
 }
